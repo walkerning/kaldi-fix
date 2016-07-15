0d6132aab2f66128b539e176bc4cf357c5ae6ac0
MasterXiong <harryfootball@163.com>
2016-07-15 16:49:34 +0800

    Add fix-strategy-test

---
+++ src/fix/fix-strategy-test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <tr1/memory>
+#include "fix/fix-strategy.h"
+#include "fix/fix-dynamic-fixed-point.h"
+#include "fix/fix-null-strategy.h"
+using namespace std;
+
+int main()
+{
+	cout << "Hello!" << endl;
+	string cases[] = {
+		// 空输入
+		"",
+		"    ",
+		// 错误输入
+		"Dynamic",
+		"<Dynamic>",
+		"<DynamicFixedPoint> <BlobInBit> 0 16",
+		// 正确输入
+		"<DynamicFixedPoint> <BlobIndexBit> 0 16 <BlobIndexBit> 1 8 <BlobIndexBit> 2 8",
+		"<DynamicFixedPoint> <ParamIndexBit> 0 16 <ParamIndexBit> 1 8 <ParamIndexBit> 2 4",
+		"<DynamicFixedPoint> <BlobIndexBit> 2 4 <ParamIndexBit> 0 16 <ParamIndexBit> 1 8 <BlobIndexBit> 4 16 <ParamIndexBit> 2 4",
+		"<DynamicFixedPoint> <BlobIndexBit> 1 32 <ParamIndexBit> 3 8 <ParamIndexBit> 0 4 <ParamIndexBit> 2 4",
+	};
+	int n = sizeof(cases) / sizeof(string);
+	
+	for (int i = 0; i < n; i++){
+		cout << "Input: " << cases[i];
+		tr1::shared_ptr<kaldi::fix::FixStrategy> strategy = kaldi::fix::FixStrategy::Init(cases[i]);
+	}
+	return 0;
+}
